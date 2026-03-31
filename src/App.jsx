64e239f50db8397c0c4762343cb3c2485499bc1a
IUB_LOKI <ayoubouharda33@gmail.com>
2026-03-31 11:23:49 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,38 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import PlateCard from './components/PlateCard'
 
-  // const plats = [
-  //   {nom: "Couscous", price: 90, is_available: true },
-  //   {nom: "Tajine", price: 120, is_available: false },
-  //   {nom: "Pastilla", price: 150, is_available: true },
-  //   {nom: "Rfissa", price: 100, is_available: true },
-  // ]
+  const plats = [
+  { id: 1, nom: "Couscous", prix: 90, disponiblilite: true },
+  { id: 2, nom: "Tajine", prix: 120, disponiblilite: false },
+  { id: 3, nom: "Pastilla", prix: 150, disponiblilite: true },
+  { id: 4, nom: "Rfissa", prix: 100, disponiblilite: true },
+]
 
-  // export default function App()
-  // {
-  //   return (
-  //     <div>
-  //       {plats.map((plat) => (
-  //         <PlateCard 
-  //           key={plat.id} 
-  //           name={plat.name} 
-  //           price={plat.price} 
-  //           is_available={plat.is_available} 
-  //         />
-  //       ))}
-  //     </div>
-  //   )
-  // }
+  export default function App()
+  {
+    return (
+      <div>
+        {plats.map((plat) => (
+          <PlateCard
+            key={plat.id}
+            name={plat.nom} 
+            price={plat.prix} 
+            is_available={plat.disponiblilite} 
+          />
+        ))}
+      </div>
+    )
+  }
 
-function App() {
+// function App() {
 
-  return (
-    <div>
-      <h1>Application de plats</h1>
+//   return (
+//     <div>
+//       <h1>Application de plats</h1>
 
-      <PlateCard nom='pizza' prix = {500} disponibilite = {true}/>
-      <PlateCard nom='tacos' prix = {45} disponibilite = {false}/>
-      <PlateCard nom='burger' prix = {49} disponibilite = {true}/>
-    </div>
-  );
-}
-
-
-export default App;
+//       <PlateCard nom='pizza' prix = {500} disponibilite = {true}/>
+//       <PlateCard nom='tacos' prix = {45} disponibilite = {false}/>
+//       <PlateCard nom='burger' prix = {49} disponibilite = {true}/>
+//     </div>
+//   );
+// }
--- src/components/PlateCard.jsx
+++ src/components/PlateCard.jsx
@@ -1,22 +1,9 @@
-//  export default function PlateCard(props) {
-//   return (
-//     <div className="plate-card">
-//       <h3>{props.name}</h3>
-//       <p>{props.price} MAD</p>
-//       {/* maintenant on va ajouter la condition qui vérifie si il est disponible*/}
-//       {props.is_available ? (<span className="badge">Disponible</span>) : (<span className="badge bg-danger">Indisponible</span>)}
-//     </div>
-//   );
-// }
-
-function PlateCard(props)
-{
-    return (
-        <div>
-            <h2>{props.nom}</h2>
-            <h3>{props.prix}</h3>
-            <h4>{props.disponibilite ? <p>disponible</p> : <p>indisponible</p>}</h4>
-        </div>
-    );
+export default function PlateCard({ name, price, is_available }) {
+  return (
+    <div className="plate-card">
+      <h3>{name}</h3>
+      <p>{price} MAD</p>
+      {is_available ? <span className="badge">Disponible</span> : <span className="badge">Indisponible</span>}
+    </div>
+  )
 }
-export default PlateCard;
